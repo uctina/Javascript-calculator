f26eefc01608c3fb70492867d18ca515fdbcb336
uctina <105360091+uctina@users.noreply.github.com>
2022-06-22 12:07:25 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var opr = prompt("Operator: ")
+var num1= parseInt(prompt("Number1: ")) 
+var num2=parseInt(prompt("Number2: ")) 
+
+if(opr == "+")
+{
+    console.log(num1 + num2)
+}
+else if(opr == "-")
+{
+    console.log(num1 - num2)
+}
+else if(opr == "*")
+{
+    console.log(num1 * num2)
+}
+else if(opr == "/")
+{
+    console.log(num1 / num2)
+}
